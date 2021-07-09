cf4e24224f2b53575c13a8a2e3f747710e0ead30
Uri Herrera <hello@uriherrera.com>
2021-07-09 11:53:23 -0500

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -5,6 +5,16 @@ set -x
 apt -qq update
 apt -qq -yy install equivs curl git wget gnupg2
 
+### Replace Travis containers' sources file
+
+wget -qO /etc/apt/sources.list.d/sources.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.debian.unstable
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+	04EE7237B7D453EC \
+	648ACFD622F3D138 > /dev/null
+
+apt -qq update
+
 ### Install Dependencies
 
 DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules
