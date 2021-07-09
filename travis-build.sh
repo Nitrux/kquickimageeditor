5344ad8e015f22dfb837ca925aff4ffc1b3e6a78
Uri Herrera <hello@uriherrera.com>
2021-07-09 12:14:33 -0500

    check if build with neon

--- travis-build.sh
+++ travis-build.sh
@@ -7,11 +7,22 @@ apt -qq -yy install equivs curl git wget gnupg2
 
 ### Replace Travis containers' sources file
 
-wget -qO /etc/apt/sources.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy-minimal/configs/files/sources.list.debian.unstable
+# wget -qO /etc/apt/sources.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy-minimal/configs/files/sources.list.debian.unstable
+
+# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+# 	04EE7237B7D453EC \
+# 	648ACFD622F3D138 > /dev/null
+
+# apt -qq update
+# apt -yy dist-upgrade
+# apt -yy autoremove
+
+### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
+
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
-	04EE7237B7D453EC \
-	648ACFD622F3D138 > /dev/null
+	55751E5D > /dev/null
 
 apt -qq update
 
