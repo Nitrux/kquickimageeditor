8e842af1c1761257f8b9b14125c847a9f05db127
Uri Herrera <hello@uriherrera.com>
2021-07-09 12:07:50 -0500

    fix file url

--- travis-build.sh
+++ travis-build.sh
@@ -7,15 +7,13 @@ apt -qq -yy install equivs curl git wget gnupg2
 
 ### Replace Travis containers' sources file
 
-wget -qO /etc/apt/sources.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.debian.unstable
+wget -qO /etc/apt/sources.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy-minimal/configs/files/sources.list.debian.unstable
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	04EE7237B7D453EC \
 	648ACFD622F3D138 > /dev/null
 
 apt -qq update
-
-apt-cache policy
 
 ### Install Dependencies
 
